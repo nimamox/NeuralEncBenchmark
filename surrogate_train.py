ebc1c3de6fbf1251bc0ff7b702664dcd73601c54
Nima Mohammadi <nimamo@vt.edu>
2021-02-11 17:31:54 -0500

    Bugfix3 global params

--- surrogate_train.py
+++ surrogate_train.py
@@ -78,7 +78,7 @@ def compute_classification_accuracy(encoded_data, nb_hidden, params, alpha, beta
         accs.append(tmp)
     return np.mean(accs)
 
-def init_model(nb_inputs, time_step, nb_hidden, tau_mem = 10e-3, tau_syn = 5e-3):
+def init_model(nb_inputs, nb_hidden, nb_output, time_step, tau_mem = 10e-3, tau_syn = 5e-3):
     global alpha, beta
     global w1, w2
 
