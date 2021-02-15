a974cc7e016a29fec3e317c46edbcf35c4e92499
Nima Mohammadi <nimamo@vt.edu>
2021-02-15 13:57:17 -0500

    Put results on gdrive

--- main.py
+++ main.py
@@ -42,8 +42,8 @@ def gen_encoded(x_train, y_train, x_test, y_test, encoder_type, grp_size, div_da
     trn_d = encode_data(x_train[mask,:].reshape(-1, 784), y_train[mask], 
                         nb_units=nb_unites, 
                         encoder_type=encoder_type, 
-                      group_size=grp_size,
-                  batch_size=512, 
+                        group_size=grp_size,
+                      batch_size=512, 
                   nb_steps=nb_steps, 
                   TMAX=TMAX, 
                   x_max_ISI=x_max_ISI, 
@@ -52,8 +52,8 @@ def gen_encoded(x_train, y_train, x_test, y_test, encoder_type, grp_size, div_da
     val_d = encode_data(x_train[~mask,:].reshape(-1, 784), y_train[~mask], 
                         nb_units=nb_unites, 
                         encoder_type=encoder_type, 
-                      group_size=grp_size,
-                  batch_size=1024, 
+                        group_size=grp_size,
+                      batch_size=1024, 
                   nb_steps=nb_steps, 
                   TMAX=TMAX, 
                   x_max_ISI=x_max_ISI, 
@@ -62,8 +62,8 @@ def gen_encoded(x_train, y_train, x_test, y_test, encoder_type, grp_size, div_da
     test_d = encode_data(x_test, y_test, 
                          nb_units=nb_unites, 
                          encoder_type=encoder_type, 
-                       group_size=grp_size,
-                  batch_size=1024, 
+                         group_size=grp_size,
+                       batch_size=1024, 
                   nb_steps=nb_steps, 
                   TMAX=TMAX, 
                   x_max_ISI=x_max_ISI, 
@@ -71,57 +71,57 @@ def gen_encoded(x_train, y_train, x_test, y_test, encoder_type, grp_size, div_da
     return trn_d, val_d, test_d
 
 confs = (
-  # -------
-    # {'gs': 1,  'enc': 'TTFS', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
+    # -------
+  # {'gs': 1,  'enc': 'TTFS', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 1,  'enc': 'TTFS', 'nb_steps': 100, 'lr': .0003, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 1,  'enc': 'TTFS', 'nb_steps': 100, 'lr': .0004, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 1,  'enc': 'TTFS', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234},
-  # -------
-    # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
+    # -------
+  # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0003, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0004, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0006, 'dataset': 'MNIST', 'seed': 1234},
-  # -------
-    # {'gs': 4,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
+    # -------
+  # {'gs': 4,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 4,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0003, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 4,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0004, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 4,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 4,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0006, 'dataset': 'MNIST', 'seed': 1234},
-  # -------
-    # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
+    # -------
+  # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0003, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0004, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0006, 'dataset': 'MNIST', 'seed': 1234},
-  # -------
-    # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
+    # -------
+  # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0003, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0004, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0006, 'dataset': 'MNIST', 'seed': 1234},
-  # -------
-    # {'gs': 8,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
+    # -------
+  # {'gs': 8,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 8,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0003, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 8,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0004, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 8,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 8,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0006, 'dataset': 'MNIST', 'seed': 1234},
-  # -------
-    # {'gs': 16,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
+    # -------
+  # {'gs': 16,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0002, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 16,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0003, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 16,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0004, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 16,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234},
     # {'gs': 16,  'enc': 'Phase+TTFS', 'nb_steps': 100, 'lr': .0006, 'dataset': 'MNIST', 'seed': 1234},
-  # -------
-    # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 5},
+    # -------
+  # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 5},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 10},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 15},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 20},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 30},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 45},
     # {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 50},
-  # -------
-    # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 10},
+    # -------
+  # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 10},
     # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 10},
     # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 50},
     # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 50},
@@ -129,8 +129,8 @@ confs = (
     # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 150},
     # {'gs': 4,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 200},
     # {'gs': 8,  'enc': 'Phase+ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'nb_hidden': 200},
-  # -------
-    {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .001, 'dataset': 'MNIST', 'seed': 1234},
+    # -------
+  {'gs': 1,  'enc': 'ISI', 'nb_steps': 100, 'lr': .0005, 'dataset': 'MNIST', 'seed': 1234, 'reg': 1e-5},
 )
 
 epochs = 30
@@ -141,12 +141,28 @@ div_data = 5
 for c in confs:
     if c['dataset'] == 'MNIST':
         dataset = load_mnist()
+    elif c['dataset'] == 'MNISTSMALL':
+        mnist = load_mnist()
+        sub_mask = np.zeros_like(mnist['y_train'], dtype=np.bool)
+        sub_mask[::10] = True
+        dataset = {}
+        dataset['x_train'] = mnist['x_train'][sub_mask,:]
+        dataset['y_train'] = mnist['y_train'][sub_mask]
+        dataset['x_test'] = mnist['x_test']
+        dataset['y_test'] = mnist['y_test']
     elif c['dataset'] == 'FMNIST':
         dataset = load_fmnist()
     elif c['dataset'] == 'CIFAR10_gray':
         dataset = load_cifar10_gray()
     else:
         raise Exception('Wrong database name')
+
+    invertdata = c.get('invertdata', False)
+    if invertdata:
+        print('Invert Dataset')
+        dataset['x_train'] = dataset['x_train']*-1+1
+        dataset['x_test'] = dataset['x_test']*-1+1
+
     print('*'* 20)
     nb_inputs  = int(dataset['x_train'].shape[1]/c['gs'])
     if not c.get('nb_hidden', False):
@@ -154,14 +170,15 @@ for c in confs:
     else:
         nb_hidden = c['nb_hidden']
     regularization_f = 0
-    if not c.get('reg', False):
+    if c.get('reg', False):
+        print("REGULARIZATION", c['reg'])
         regularization_f = c['reg']
     nb_outputs = 10
 
     torch.manual_seed(c['seed'])
     np.random.seed(c['seed'])
     random.seed(c['seed'])
-    
+
     tmx = c.get('tmx', 100)
     misi = c.get('misi', 255)
     oisi = c.get('oisi', 0)
@@ -181,17 +198,27 @@ for c in confs:
     print("#weights:", total_weights)
 
     loss_hist, train_acc, val_acc, w_traj = train(trn_d, val_d, c['nb_steps'], params, alpha, beta, 
-                                                  lr=c['lr'], nb_epochs=epochs, return_weights=True)
+                                                  lr=c['lr'], nb_epochs=epochs, return_weights=True, 
+                                                  regularization_factor=regularization_f)
     test_acc = compute_classification_accuracy(test_d, c['nb_steps'], params, alpha, beta)
     print("Test accuracy: %.3f" % test_acc)
 
-    fpath = 'SSG_{}__{}__gs{}_i{}_h{}_tot{}__epochs{}_lr{}_div{}_nbs{}_ts{}__seed{}_tmx{}_misi{}_oisi{}_reg{}.h'.format(
+    if regularization_f:
+        rr = '_reg{}.h'.format(regularization_f)
+    else:
+        rr = ''
+
+    invv = ''
+    if invertdata:
+        invv = '_invertdata'
+
+    fpath = 'SSG_{}__{}__gs{}_i{}_h{}_tot{}__epochs{}_lr{}_div{}_nbs{}_ts{}__seed{}_tmx{}_misi{}_oisi{}{}{}'.format(
         'MNIST', c['enc'].replace('+', ''), c['gs'], nb_inputs, nb_hidden, total_weights, epochs, 
-      str(c['lr']).replace('.', '_'), div_data, c['nb_steps'], time_step, c['seed'], tmx, misi, oisi, regularization_f
+        str(c['lr']).replace('.', '_'), div_data, c['nb_steps'], time_step, c['seed'], tmx, misi, oisi, rr, invv
     )
     with open('SurrGradResults/' + fpath, 'wb') as fo:
         pickle.dump({
-          'train_acc': train_acc,
+            'train_acc': train_acc,
           'loss_hist': loss_hist,
           'val_acc': val_acc,
           'test_acc': test_acc,
@@ -212,6 +239,7 @@ for c in confs:
           'misi': misi,
           'oisi': oisi,
           'reg': regularization_f,
+          'invertdata': invertdata
           }, fo, protocol=pickle.HIGHEST_PROTOCOL)
         fo.flush()
     print(fpath)
